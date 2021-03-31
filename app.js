a1af9342bba7325849c9a638e876932031b257c9
00008878 <00010351wiut@gmail.com>
2021-03-31 11:08:46 +0500

    created layout page

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})

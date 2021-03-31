19d2280531cead08de43c283288143b257147249
00008878 <00010351wiut@gmail.com>
2021-03-31 11:34:32 +0500

    create function

--- app.js
+++ app.js
@@ -11,3 +11,45 @@ app.use(express.urlencoded({extended: false}))
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const date = req.body.date
+    const experience = req.body.experience
+    const about = req.body.about
+
+    fs.readFile('./data/hired.json', (err, data) => {
+        if (err) throw err
+
+        const employees = JSON.parse(data)
+
+        employees.push({
+            id: id(),
+            name: name,
+            date: date,
+            experience: experience,
+            about: about,
+        })
+
+        fs.writeFile('./data/hired.json', JSON.stringify(employees), err => {
+         if (err) throw err
+        })
+        res.redirect('/create?success=1')
+    })
+})
+
+
+app.listen(3000,  err => {
+    if(err) console.log(err)
+
+    console.log('Server is running on port 3000...')
+})
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
